7ef6243fc81df9a0e22f058b979da0324c507bb4
GussGtz <gutierrezgustavocanul@gmail.com>
2025-07-23 12:09:32 -0600

    Agrego bd

--- config/db.js
+++ config/db.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // asegúrate de que PORT esté en tu .env
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: true // 🔒 Requerido por Aiven para conexiones SSL
-  }
+  ssl: process.env.DB_SSL === 'true' ? {
+    rejectUnauthorized: true
+  } : false
 });
 
 module.exports = pool;
